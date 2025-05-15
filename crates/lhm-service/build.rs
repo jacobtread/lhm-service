d228d20d214f6bfa7a1a329084cecfc75b2adc0d
Jacobtread <jacobtread@gmail.com>
2025-05-15 15:44:01 +1200

    feat: remove now unused extra build flags for service

--- crates/lhm-service/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    println!("cargo:rustc-flags=-l ole32");
-    println!("cargo:rustc-link-lib=dylib=Iphlpapi");
-    println!("cargo:rustc-link-lib=dylib=bcrypt");
-    println!("cargo:rustc-link-lib=dylib=OleAut32");
-    println!("cargo:rustc-link-lib=dylib=Crypt32");
-}
